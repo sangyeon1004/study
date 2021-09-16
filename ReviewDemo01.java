dbb0db83f3a6927eaf463d44b7cb16542d1f361f
sangyeon1004 <yean_1004@naver.com>
2021-09-17 03:56:39 +0900

    v03

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-//02
+//03
 public class ReviewDemo01 {
     public static void main(String[] args) {
         //소수:1과 자기자신외에는 나누어 떨어지지 않는 수
@@ -9,9 +9,9 @@ public class ReviewDemo01 {
         //int cnt = 0;
         boolean isPrime=true;
         for (int k = 2; k < number; k++){ //반복 횟수 2회 감소
-            if (number % k == 0) //입력된 수를 k로 나누었을 때 나머지가 0인경우.즉,약수
-                // cnt++; //cut=cut+1
+            if (number % k == 0)
                 isPrime = false;
+
 
         }
         if(isPrime==true) //isPrime 변수 값이 true이면
@@ -21,7 +21,28 @@ public class ReviewDemo01 {
     }
 }
 
+/*02
+public class ReviewDemo01 {
+    public static void main(String[] args) {
+        //소수:1과 자기자신외에는 나누어 떨어지지 않는 수
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        //int cnt = 0;
+        boolean isPrime=true;
+        for (int k = 2; k < number; k++){ //반복 횟수 2회 감소
+            if (number % k == 0) //입력된 수를 k로 나누었을 때 나머지가 0인경우.즉,약수
+               // cnt++; //cut=cut+1
+                isPrime = false;
 
+    }
+    if(isPrime==true) //isPrime 변수 값이 true이면
+        System.out.println(number+"은 소수!");
+    else
+        System.out.println(number + "은 소수가 아닙니다");
+}
+}
+
+*/
 /*01
 public class ReviewDemo01 {
     public static void main(String[] args) {
