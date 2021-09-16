f3b088a5648943db017128cd51c0175a96c65171
sangyeon1004 <yean_1004@naver.com>
2021-09-17 02:15:32 +0900

    v01

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,98 +1,7 @@
 import java.util.Scanner;
 
-//06
-public class ReviewDemo01 {
-    public static void main(String[] args) {
-        //소수:1과 자기자신외에는 나누어 떨어지지 않는 수
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        //int cnt = 0;
-        boolean isPrime=true;
-        for (int k = 2; k < number; k++){ //반복 횟수 2회 감소
-            if (number % k == 0) {
-                isPrime = false;
-                break; //첫번째 약수가 발견되면 for문 탈출
-            }
-            String str=null;
-            k++;
-        }
-        System.out.println(number + (isPrime ? "은소수!" : "소수가 아니다"));
-    }
-}
-/*/05
-public class ReviewDemo01 {
-    public static void main(String[] args) {
-        //소수:1과 자기자신외에는 나누어 떨어지지 않는 수
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        //int cnt = 0;
-        boolean isPrime=true;
-        for (int k = 2; k < number; k++){ //반복 횟수 2회 감소
-            if (number % k == 0) {
-                isPrime = false;
-                break; //첫번째 약수가 발견되면 for문 탈출
-            }
-        }
-        int k=2;
-        while(k<number){
-        if(number % k==0){
-        isPrime=false;
-        break;
-        }
-        k++;
-        }
-        if(isPrime==true) //isPrime 변수 값이 true이면
-            System.out.println(number+"은 소수!");
-        else
-            System.out.println(number + "은 소수가 아닙니다");
-    }
-}
 
- */
-/*03
-public class ReviewDemo01 {
-    public static void main(String[] args) {
-        //소수:1과 자기자신외에는 나누어 떨어지지 않는 수
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        //int cnt = 0;
-        boolean isPrime=true;
-        for (int k = 2; k < number; k++){ //반복 횟수 2회 감소
-            if (number % k == 0)
-                isPrime = false;
-
-
-        }
-        if(isPrime==true) //isPrime 변수 값이 true이면
-            System.out.println(number+"은 소수!");
-        else
-            System.out.println(number + "은 소수가 아닙니다");
-    }
-}
-
-/*02
-public class ReviewDemo01 {
-    public static void main(String[] args) {
-        //소수:1과 자기자신외에는 나누어 떨어지지 않는 수
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        //int cnt = 0;
-        boolean isPrime=true;
-        for (int k = 2; k < number; k++){ //반복 횟수 2회 감소
-            if (number % k == 0) //입력된 수를 k로 나누었을 때 나머지가 0인경우.즉,약수
-               // cnt++; //cut=cut+1
-                isPrime = false;
-
-    }
-    if(isPrime==true) //isPrime 변수 값이 true이면
-        System.out.println(number+"은 소수!");
-    else
-        System.out.println(number + "은 소수가 아닙니다");
-}
-}
-
-*/
-/*01
+//01
 public class ReviewDemo01 {
     public static void main(String[] args) {
         //소수:1과 자기자신외에는 나누어 떨어지지 않는 수
@@ -110,4 +19,3 @@ public class ReviewDemo01 {
 }
 }
 
- */
