0498b7d696fc01a0792200ca11cd4bae38258635
sangyeon1004 <yean_1004@naver.com>
2021-09-17 03:53:48 +0900

    v02

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
 
+//02
+public class ReviewDemo01 {
+    public static void main(String[] args) {
+        //소수:1과 자기자신외에는 나누어 떨어지지 않는 수
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        //int cnt = 0;
+        boolean isPrime=true;
+        for (int k = 2; k < number; k++){ //반복 횟수 2회 감소
+            if (number % k == 0) //입력된 수를 k로 나누었을 때 나머지가 0인경우.즉,약수
+                // cnt++; //cut=cut+1
+                isPrime = false;
 
-//01
+        }
+        if(isPrime==true) //isPrime 변수 값이 true이면
+            System.out.println(number+"은 소수!");
+        else
+            System.out.println(number + "은 소수가 아닙니다");
+    }
+}
+
+
+/*01
 public class ReviewDemo01 {
     public static void main(String[] args) {
         //소수:1과 자기자신외에는 나누어 떨어지지 않는 수
@@ -11,8 +32,6 @@ public class ReviewDemo01 {
         for (int k = 1; k <= number; k++){
             if (number % k == 0)
                 cnt++;
-
-
     }
     if(cnt==2)
         System.out.println(number+"은 소수!");
@@ -21,3 +40,4 @@ public class ReviewDemo01 {
 }
 }
 
+ */
